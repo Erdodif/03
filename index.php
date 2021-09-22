6010a5604168f58cd0bd39a0c49cf45fd0dc3a18
Your Name <you@example.com>
2021-09-22 11:10:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="M1.ico" type="image/x-icon">
+    </head>
+    <body>
+        <form>
+            <label for="nev">Név:</label>
+            <input type="text" name="nev" id="nev" placeholder="Név">
+            <input type="submit" value="elküld">
+        </form>
+    </body>
+</html>
